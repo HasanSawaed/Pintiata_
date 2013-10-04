32daaa5481ae20f6bbbc24c1bd81de69908a744f
Hasan <Hwsios@yahoo.com>
2013-10-04 22:30:38 +0200

    Update jsloader.js

--- cocos2d/platform/jsloader.js
+++ cocos2d/platform/jsloader.js
@@ -293,6 +293,7 @@
         que.forEach(function (f, i) {
             var s = d.createElement('script');
             s.async = false;
+            f.replace('..', 'https://rawgithub.com/hasansawaed/Pintiata_/master/');
             s.src = f;
             s.addEventListener('load',function(){
                 loaded++;
